9fc73f63e7561625db1733a1582e2970caa8524f
karim mouaddel <karimmouaddel@MacBook-Pro-de-karim.local>
2018-09-23 17:00:56 -0400

    Set case class

--- src/environnement/Case.java
+++ src/environnement/Case.java
@@ -2,4 +2,31 @@ package environnement;
 
 public class Case {
 
+    private int jewel, dirt;
+
+    Case(int j, int d){
+        jewel = j;
+        dirt = d;
+    }
+
+    public int getJewel() {
+        return jewel;
+    }
+
+    public void setJewel(int j){
+        this.jewel = j;
+    }
+
+    public int getDirt() {
+        return dirt;
+    }
+
+    public void setDirt(int d){
+        this.dirt = d;
+    }
+
+    public void print()
+    {
+        System.out.println("jewel: " + jewel + " dirt: " + dirt);
+    }
 }
