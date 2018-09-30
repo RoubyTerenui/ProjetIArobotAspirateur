ef27fe22232bd63ef434ef1539475285ef50b62d
Unknown <terenuirouby@live.fr>
2018-09-30 18:03:19 -0400

    Conflict resolved

--- src/Test/TestEffectors.java
+++ src/Test/TestEffectors.java
@@ -1,5 +1,13 @@
 package Test;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 public class TestEffectors {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 
 }
--- src/environnement/Grid.java
+++ src/environnement/Grid.java
@@ -3,10 +3,13 @@ package environnement;
 public class Grid implements Runnable {//Grid of the environment with the functions that generate randomly dust and jewels
 	Box[][] grid;
 	int mesureDePerformance;
-	
+	boolean environmentRunning;
 	
 	//Constructor
 	public Grid() {
+		mesureDePerformance=0;
+		environmentRunning=true;
+		grid=new Box [10][10];
 		for (int i=0;i<10;i++) {
 			for(int j=0;j<10;j++) {
 				grid[i][j]=new Box(0,0);
@@ -27,7 +30,7 @@ public class Grid implements Runnable {//Grid of the environment with the functi
 	}
 	//Other Methods
 	public void generateEnvironment(){
-		for (int i=0;i<2;i++) {
+		for (int i=0;i<10;i++) {
 			for(int j=0;j<10;j++) {
 				grid[i][j].generateJewel();
 				grid[i][j].generateDirt();
@@ -36,7 +39,9 @@ public class Grid implements Runnable {//Grid of the environment with the functi
 	}
 	@Override
 	public void run() {//Method that indicate what the thread will do
-		// TODO Auto-generated method stub
-		
+		while (environmentRunning) {
+
+			generateEnvironment();
+		}
 	}
 }
